cd552c6d9ed21507498ebb25ce699928eff96049
Pablo Mitchell <pablo.mitchell@gmail.com>
2021-02-18 15:19:07 -0800

    Building out package structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='liner-notes',
+    version='0.1',
+    packages=['liner-notes', 'liner-notes.data'],
+    url='https://github.com/pablomitchell/liner-notes',
+    license='',
+    author='pablo mitchell',
+    author_email='pablo.mitchell@gmail.com',
+    description='Generate Liner Notes'
+)
